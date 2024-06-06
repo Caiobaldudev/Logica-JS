31cb4acbbc33408607c266551c44e58cfb24921b
Caio Balduino <caiobalduinodev@hotmail.com>
2024-06-06 18:41:21 -0300

    example_6_cap_1

---
+++ Capitulo_1/ex1_6/script.js
@@ -0,0 +1,6 @@
+ //Elaboração de um programa que leia dois números, calcule e informe a soma deles.
+ const num1 = Number(prompt('1º Número: ')) //lê o 1 numero
+ const num2 = Number(prompt('2º Número: ')) //lê o 2 numero
+ const soma = num1 + num2 //Calcula a soma
+ alert('A soma de ' + num1 + ' e ' + num2 + ' é ' + soma) //Exibe o resultado
+ 
