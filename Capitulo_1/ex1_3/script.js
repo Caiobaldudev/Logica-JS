dbb1e01a454a7df76017fe58259f6aa6d8e1314d
Caio Balduino <caiobalduinodev@hotmail.com>
2024-06-06 18:36:19 -0300

    example_3_cap_1

---
+++ Capitulo_1/ex1_3/script.js
@@ -0,0 +1,9 @@
+const a = "20";
+const b = a * 2; //b = 40
+const c = a / 2; //c = 10
+const d = a - 2; //d = 18
+const e = a + 2; //e = 202
+alert("e: " + e); //exibe um valor de uma variavel.
+
+// Para converter o número em 'a' podemos utilizar o método .Number() ficando assim:
+const f = Number(a) + 2; //e = 22;
