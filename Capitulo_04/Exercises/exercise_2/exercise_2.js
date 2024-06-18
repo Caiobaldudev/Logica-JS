8fddaba47c4b9fac8a02b14b6c01c795e2ef16f5
Caio Balduino <caiobalduinodev@hotmail.com>
2024-06-17 23:09:56 -0300

    exercise_2_cap_4_velocity

---
+++ Capitulo_04/Exercises/exercise_2/exercise_2.js
@@ -0,0 +1,29 @@
+// Elaborar um programa que leia um numero. Informe se ele é par ou ímpar.
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //leitura da velocidades
+  const permitido = Number(frm.inPermitido.value)
+  const condutor = Number(frm.inCondutor.value)
+  //Condições
+  let velocidadeAcimaPermitida = (condutor - permitido)
+  let porcentagem = (permitido * 0.2)
+  
+  if((frm.inCondutor.value == '' || frm.inPermitido.value == '') || (isNaN(permitido) || isNaN(condutor))) {
+    alert('Digite uma velocidade válida!')
+    frm.inPermitido.value = ''
+    frm.inCondutor.value = ''
+    condutor.focus();
+    return
+  }
+  if (condutor <= permitido) {
+    resp.innerText = `Situação: Sem Multa!`
+  } else if(velocidadeAcimaPermitida >= porcentagem) {
+    resp.innerText = `Situação: Multa Grave!`
+  } else {
+    resp.innerText = `Situação: Multa Leve!`
+  }
+});
+
